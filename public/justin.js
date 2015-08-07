898d28e2674ecbb0cb11855be6d3a379e5023452
Justin <jethielke@gmail.com>
2015-08-07 10:30:59 -0600

    finished

--- public/justin.js
+++ public/justin.js
@@ -1,175 +1,8 @@
-// (function (global) {
-//   var CYCLE_SPEED = 1000
-//       ,HELIX_WIDTH = 600
-//       ,SEGMENT_BUFFER = 0.2
-//       ,btnHelixStart
-//       ,helixIsInitted
-//       ,helixIsRunning
-//       ,startTime
-//       ,pausedAtTime
-//       ,segmentContainer
-//       ,segments
-//       ,stepStateLists
-//       ,updateHandle
-//       ,isPlaying
-//       ,shiftyDemo
-//       ,i;
-
-//   function now () {
-//     return +(new Date());
-//   }
-
-//   function getPosition (loopPosition, startBuffer) {
-//     var moddedLoopPosition
-//       ,interpolator
-//       ,interpolatedValue;
-
-//     if ((loopPosition - startBuffer) < 0) {
-//       return 0;
-//     }
-
-//     interpolator = moddedLoopPosition = ((loopPosition - startBuffer));
-
-//     if (moddedLoopPosition > 1) {
-//       interpolator = moddedLoopPosition - 1;
-//     }
-
-//     interpolatedValue = global.Tweenable.interpolate(
-//         {'left': 0}, {'left': HELIX_WIDTH }, interpolator, 'easeInOutSine');
-
-//     if (moddedLoopPosition > 1) {
-//       return {
-//         'left': HELIX_WIDTH - interpolatedValue.left
-//       };
-//     } else {
-//       return interpolatedValue;
-//     }
-//   }
-
-//   function updateSegment (index, loopPosition) {
-//     var segment
-//       ,calculated;
-
-//     segment = segments[index];
-//     calculated = getPosition(loopPosition, index * SEGMENT_BUFFER);
-
-//     var style = segment.style;
-//     style.MozTransform = style.webkitTransform =
-//       'translateX(' + parseFloat(calculated.left) + 'px)';
-//   }
-
-//   function updateSegments (startTime, callback) {
-//     var i
-//       ,timeDelta
-//       ,normalizedTime;
-
-//     timeDelta = now() - startTime;
-//     normalizedTime = timeDelta / CYCLE_SPEED;
-
-//     for (i = 0; i < segments.length; i++) {
-//       updateSegment(i, normalizedTime);
-//     }
-
-//     callback();
-//   }
-
-//   function loop () {
-//     updateHandle = setTimeout(function () {
-//       updateSegments(startTime, loop);
-//     }, 1 / 60);
-//   }
-
-//   function pause () {
-//     if (isPlaying === false) {
-//       return;
-//     }
-
-//     isPlaying = false;
-//     pausedAtTime = Tweenable.now();
-//     clearTimeout(updateHandle);
-//   }
-
-//   function play () {
-//     if (isPlaying === true) {
-//       return;
-//     }
-
-//     isPlaying = true;
-//     startTime = Tweenable.now() - (pausedAtTime - startTime);
-//     loop();
-
-
-//   }
-
-//   function init (containerId) {
-//     pausedAtTime = startTime = Tweenable.now();
-//     segmentContainer = document.getElementById(containerId);
-//     segments = segmentContainer.children;
-//     stepStateLists = [];
-
-//     for (i = 0; i < segments.length; i++) {
-//       stepStateLists.push([])
-//     }
-
-//     window.pause = pause;
-//     window.play = play;
-//   }
-
-//   function toggleHelix () {
-
-//     if (!helixIsInitted) {
-//       helixIsInitted = true;
-//       shiftyDemo.helixInit('helix-container');
-//     }
-
-//     if (helixIsRunning) {
-//       shiftyDemo.helixPause();
-//       btnHelixStart.innerHTML = 'Restart Game!';
-//     } else {
-//       shiftyDemo.helixPlay();
-//       btnHelixStart.innerHTML = 'Stop Game!';
-//     }
-
-//     helixIsRunning = !helixIsRunning;
-//   }
-
-//   shiftyDemo = {
-//     'helixInit': init
-//     ,'helixPlay': play
-//     ,'helixPause': pause
-//   };
-
-//   btnHelixStart = document.getElementById('btnHelixStart');
-//   btnHelixStart.addEventListener('click', toggleHelix, true);
-//   helixIsRunning = false;
-//   helixIsInitted = false;
-// } (this));
-
-
-
-
-
-
-
-// 	$(document).on('ready', function(){
-
-// 		$('.segment').on('click', function(event){
-//       event.preventDefault()
-// 			$(this).hide()
-//       alert('You Won!')
-// 		});
-// 	});
-
-
-
-
-
-
+var tweenable = new Tweenable();
 
 var textOne = function(){
 
-var text=document.getElementById('text1')
-
+        var text=document.getElementById('text1')
         , style=text.style
         , myTweenable = new Tweenable()
         , update
@@ -221,7 +54,7 @@ var textTwo = function(){
 
 var textThree = function(){
 
-var text=document.getElementById('text3')
+        var text=document.getElementById('text3')
         , style=text.style
         , myTweenable = new Tweenable()
         , update
@@ -270,10 +103,26 @@ var textFour = function(){
         loop();
 }
 
+
+        var output = document.getElementById('text1')
+        tweenable.tween({
+          from: { color: '#41ff88' },
+          to:   { color: '#f0f' },
+          duration: 10000,
+          easing: 'easeOutQuart',
+          step: function (mystate) {
+                output.style.color = mystate.color;
+          }
+        });
+
+
+
+
 textOne()
 textTwo()
 textThree()
 textFour()
+
 
 
 
